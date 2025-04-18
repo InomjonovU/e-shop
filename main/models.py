523c0b72693649224b5eac8da7278119fd0dfc64
Umidjon Inomjonov <vikenzo.me@gmail.com>
2025-04-18 22:12:47 +0500

    commit

---
+++ main/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ['-created_at']
+
+class Enter(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Enter'
+        verbose_name_plural = 'Enters'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.product.quantity += self.quantity
+        self.product.save()
+
+class Out(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Out'
+        verbose_name_plural = 'Outs'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.product.quantity -= self.quantity
+        self.product.save()
